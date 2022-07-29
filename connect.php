5b0820656d7498dcc48b0698029195c305da79bc
Tamim <86544999+TamimHq@users.noreply.github.com>
2022-07-29 20:29:59 +0600

    Add files via upload

--- connect.php
+++ connect.php
@@ -17,6 +17,25 @@
    }
    else
    {
-      echo "there is an problem".mysqli_error($db);
+      echo "there is an problem";
    }
+   $sql2="CREATE TABLE Admin(ID INT NOT NULL PRIMARY KEY, NAME VARCHAR(30) NOT NULL,EMAIL VARCHAR(30) NOT NULL, PASSWORD VARCHAR(30) NOT NULL)";
+   if(mysqli_query($db,$sql2))
+   {
+      echo "table created";
+   }
+   else
+   {
+      echo "there is an problem";
+  }
+   $sql3="INSERT INTO Admin(ID,NAME,EMAIL,PASSWORD) VALUES(1,'Akash','akash@xyz.com','1234'), (2,'Tamim','tamim@xyz.com','4567'),(3,'Sadman','sadman@xyz.com','6789')";
+     if(mysqli_query($db,$sql3))
+   {
+      echo "DATA ENTERED";
+   }
+   else
+   {
+      echo "there is an problem".mysqli_errot($db);
+   }
+
 ?>
