c0cf83d07a2f14bbbda1c128599df04255e6d57f
Tamim <86544999+TamimHq@users.noreply.github.com>
2022-08-25 20:44:19 +0600

    Add files via upload

--- admin_login.php
+++ admin_login.php
@@ -1,24 +1,30 @@
 <?php
-   
+     session_start();
+
     $db = mysqli_connect("localhost","root","","telecommunication_and_management_system");
    if(!$db)
    {
     die ("database not connected".mysqli_connect_error($db));
    }
-
+  
     if(isset($_POST['submit']))
   {
      $admin_email=$_POST['email'];
      $admin_password=$_POST['password'];
 
       $sql2="SELECT * FROM admin WHERE EMAIL='$admin_email' AND PASSWORD='$admin_password' ";
-
-     $result = mysqli_query($db,$sql2);
+      
+    
+    $result = mysqli_query($db,$sql2);
+    $record = mysqli_fetch_assoc($result);
     if (mysqli_num_rows($result)==1) {
+      $_SESSION['EMAIL']=$admin_email;
+      $_SESSION['NAME']=$record['NAME'];
+
       ?>
       <script type="text/javascript" >
         alert("Log-in success")
-        window.open("http://localhost/telecommunication%20Management%20System/viewcrud.php","_self");
+        window.open("http://localhost/Telecommunication%20Management%20System/index2.php","_self");
 
         </script>
 
@@ -27,7 +33,14 @@
 
      else
      {
-      echo "problem";
+      ?>
+      <script type="text/javascript" >
+        alert("There is a problem")
+        window.open("http://localhost/Telecommunication%20Management%20System/admin.html","_self");
+
+        </script>
+        <?php
+
      }
 
   }
